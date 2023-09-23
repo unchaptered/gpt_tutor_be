6f9fdbfdf33c4d88ccf6f1ed0c53b41cf12cff68
unchaptered <workstation19961002@gmail.com>
2023-09-23 21:23:55 +0900

    Create : RdsConfigType

--- src/common/types/config/config_type.py
+++ src/common/types/config/config_type.py
@@ -5,5 +5,15 @@ class OpenaiConfigType(TypedDict):
     API_KEY: str
 
 
+class RdsConfigType(TypedDict):
+    HOST: str
+    PORT: int
+    USER: str
+    PWD: str
+    DB_NAME: str
+    DB_CONNECTION_LIMIT: int
+
+
 class ConfigType(TypedDict):
     OPENAI: OpenaiConfigType
+    RDS: RdsConfigType
