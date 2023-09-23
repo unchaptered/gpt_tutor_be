c8422ea200b9c4b8a5b4fc08c04f4120ab7f8017
unchaptered <workstation19961002@gmail.com>
2023-09-23 21:24:08 +0900

    Create : RdsProvider

---
+++ src/aws/rds_provider.py
@@ -0,0 +1,31 @@
+from mysql.connector import connect, MySQLConnection
+from contextlib import contextmanager
+from typing import Generator
+
+# Modules
+from utilities.config_provider import configProvider
+
+envInstance = configProvider.getConfig()
+
+
+conf = {
+    'host': envInstance['RDS']['HOST'],
+    'port': 3306,
+    'user': envInstance['RDS']['USER'],
+    'password': envInstance['RDS']['PWD'],
+    'database': envInstance['RDS']['DB_NAME']
+}
+
+
+class RdsProvider():
+
+    @contextmanager
+    def get_connection(self) -> Generator[MySQLConnection, None, None]:
+        connection: MySQLConnection = connect(**conf)  # type: ignore
+
+        try:
+            yield connection
+        except Exception as e:
+            raise e
+        finally:
+            connection.close()
